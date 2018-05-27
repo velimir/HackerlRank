dc6e22dfae4e1b8bca3cec75dd7cdfd7fdecc410
Grigory Starinkin <starinkin@gmail.com>
2018-05-27 17:18:56 +0100

    add anagram distance with better space complexity

--- src/hackerlrank_strings.erl
+++ src/hackerlrank_strings.erl
@@ -3,7 +3,8 @@
 -include_lib("eunit/include/eunit.hrl").
 
 %% API
--export([anagram_distance/2]).
+-export([anagram_distance/2,
+         anagram_distance2/2]).
 
 %%%===================================================================
 %%% API
@@ -12,9 +13,34 @@
 anagram_distance(StrA, StrB) ->
     anagram_distance(lists:sort(StrA), lists:sort(StrB), 0).
 
+anagram_distance2(StrA, StrB) ->
+    Counters = dict:new(),
+    Counters2 = lists:foldl(fun inc_count/2, Counters, StrA),
+    Counters3 = lists:foldl(fun dec_count/2, Counters2, StrB),
+    reduce_diff(Counters3).
+
 %%%===================================================================
 %%% Internal functions
 %%%===================================================================
+inc_count(Char, Counters) ->
+    update_counter(Char, 1, Counters).
+
+dec_count(Char, Counters) ->
+    update_counter(Char, -1, Counters).
+
+update_counter(Char, Increment, Counters) ->
+    dict:update_counter(Char, Increment, Counters).
+
+reduce_diff(Counters) ->
+    dict:fold(
+      fun
+          (_Key, 0 = _Value, Acc) ->
+              Acc;
+          (_Key, Value, Acc) ->
+              abs(Value) + Acc
+      end,
+      0,
+      Counters).
 
 anagram_distance([H | TailA], [H | TailB], Acc) ->
     anagram_distance(TailA, TailB, Acc);
@@ -41,4 +67,15 @@ anagram_distance_test_() ->
      ?_assertEqual(0, anagram_distance("", "")),
      ?_assertEqual(3, anagram_distance("abc", "")),
      ?_assertEqual(3, anagram_distance("", "abc"))
+    ].
+
+
+anagram_distance2_test_() ->
+    [
+     ?_assertEqual(0, anagram_distance2("abc", "abc")),
+     ?_assertEqual(2, anagram_distance2("abb", "abc")),
+     ?_assertEqual(4, anagram_distance2("cde", "abc")),
+     ?_assertEqual(0, anagram_distance2("", "")),
+     ?_assertEqual(3, anagram_distance2("abc", "")),
+     ?_assertEqual(3, anagram_distance2("", "abc"))
     ].
